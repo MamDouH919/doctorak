0c8d233d22c055f630d418f842d2d7d2a7b0a442
Mamdouh <mamdouh.mohammed919@gmail.com>
2025-06-28 02:05:24 +0300

    allow cors

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,28 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  /* config options here */
+  async headers() {
+    return [
+      {
+        // Apply these headers to any route under `/api`
+        source: "/api/doctor", // This applies to all API routes dynamically
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "false" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Or specify a specific domain for security
+          { key: "Access-Control-Allow-Methods", value: "GET" },
+          { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
+        ]
+      }
+    ]
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ], // Add your image domain
+  },
 };
 
 export default nextConfig;
