6e756d7e9778f4de1401a947850f3715cb99a6b1
Mamdouh <mamdouh.mohammed919@gmail.com>
2025-07-06 02:09:58 +0300

    fix error

--- app/[locale]/(website)/doctors/[id]/page.tsx
+++ app/[locale]/(website)/doctors/[id]/page.tsx
@@ -3,10 +3,8 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 import Doctor from './__pageContent';
 
-const Page = ({ params }: { params: { id: string } }) => {
-    const id = params.id
-   
-
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+    const { id } = await params
     return (
         <Doctor id={id} />
     )
